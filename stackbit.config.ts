10d21b7aec8859e968120eae22d9f48bdd7b6ae7
kibenwu <kiben4@gmail.com>
2025-02-08 01:37:45 +0800

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,4 +1,5 @@
 import { defineStackbitConfig } from '@stackbit/types';
+import { GitContentSource } from "@stackbit/cms-git";
 
 export default defineStackbitConfig({
     "stackbitVersion": "~0.6.0",
@@ -6,4 +7,22 @@ export default defineStackbitConfig({
     "ssgName": "hugo",
     "contentSources": [],
     "postInstallCommand": "npm i --no-save @stackbit/types"
+
+    // ...
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDirs: ["content"],
+      models: [
+        {
+          name: "Page",
+          // Define the model as a page model
+          type: "page",
+          urlPath: "/{slug}",
+          filePath: "content/pages/{slug}.json",
+          fields: [{ name: "title", type: "string", required: true }]
+        }
+      ],
+    })
+  ]
 })
